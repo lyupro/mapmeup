8c26261a1ad207a83997aace41fb66be87872a2a
777 <lyupro21@gmail.com>
2019-07-19 01:14:08 +0300

    Added Location Model

--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'latitude',
+        'longitude',
+        'type'
+    ];
 }
